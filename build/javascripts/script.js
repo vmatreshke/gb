28dbc6e725f9febbfa64b2f6fad8e2d02390b039
akella <akella.a@gmail.com>
2013-07-25 18:18:14 +0300

    final rating

--- build/javascripts/script.js
+++ build/javascripts/script.js
@@ -564,10 +564,10 @@
 
    // alert('a');
 
-   jQuery('.parallax-layer').parallax({
-        mouseport: jQuery(".exam"),
-        xparallax: false
-      });
+var scrollorama = $.scrollorama({ blocks:'.parallax-layer' });
+scrollorama
+	.animate('#layer1',{ duration:1000, property:'top', start:0, end: 180 })
+	.animate('#layer2',{ duration: 1000, property:'top', start:0, end: 100 })
 
 }(jQuery));
 (function ($) {
--- source/javascripts/script/modules/parallax.js
+++ source/javascripts/script/modules/parallax.js
@@ -2,9 +2,9 @@
 
    // alert('a');
 
-   jQuery('.parallax-layer').parallax({
-        mouseport: jQuery(".exam"),
-        xparallax: false
-      });
+var scrollorama = $.scrollorama({ blocks:'.parallax-layer' });
+scrollorama
+	.animate('#layer1',{ duration:1000, property:'top', start:0, end: 180 })
+	.animate('#layer2',{ duration: 1000, property:'top', start:0, end: 100 })
 
 }(jQuery));
