0b9826fc811f4bfaf57efcf4d8cf9b65f4b148cb
akella <akella.a@gmail.com>
2013-07-25 18:27:46 +0300

    up

--- build/javascripts/script.js
+++ build/javascripts/script.js
@@ -566,8 +566,8 @@
 
 var scrollorama = $.scrollorama({ blocks:'.parallax-layer' });
 scrollorama
-	.animate('#layer1',{ duration:1000, property:'top', start:0, end: 180 })
-	.animate('#layer2',{ duration: 1000, property:'top', start:0, end: 100 })
+	.animate('#layer1',{ duration:1000, property:'top', start:-100, end: 80 })
+	.animate('#layer2',{ duration: 1000, property:'top', start:-100, end: 0 })
 
 }(jQuery));
 (function ($) {
