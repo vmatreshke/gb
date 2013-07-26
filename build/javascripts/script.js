b8b307c88e956d3fa2cff8196e4b11b3a9591a32
akella <akella.a@gmail.com>
2013-07-26 10:09:41 +0300

    up

--- build/javascripts/script.js
+++ build/javascripts/script.js
@@ -563,14 +563,14 @@
 (function ($) {
 
    // alert('a');
-
+if($(".parallax-layer").length>0){
 var scrollorama = $.scrollorama({ blocks:'.parallax-layer' });
 scrollorama
 	.animate('#layer1',{ duration:1000, property:'top', start:80, end: -100 })
 	.animate('#layer2',{ duration: 1000, property:'top', start:0, end: -100 })
 	// .animate('#layer1',{ duration:1000, property:'top', start:-100, end: 80 })
 	// .animate('#layer2',{ duration: 1000, property:'top', start:-100, end: 0 })
-
+}
 }(jQuery));
 (function ($) {
 
