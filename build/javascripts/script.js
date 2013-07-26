6703c51e7938ce55a03d51806280a2e376e9d04f
akella <akella.a@gmail.com>
2013-07-27 01:19:01 +0300

    final

--- build/javascripts/script.js
+++ build/javascripts/script.js
@@ -569,6 +569,12 @@ var scrollorama = $.scrollorama({ blocks:'.parallax-layer' });
 scrollorama
 	.animate('#layer1',{ duration:1000, property:'top', start:80, end: -100 })
 	.animate('#layer2',{ duration: 1000, property:'top', start:0, end: -100 })
+
+	// change 180 to 280 and -400 to -1200 to make it faster
+	//.animate('#layer1',{ duration:1000, property:'top', start:180, end: -400 })
+
+	// .animate('#layer1',{ duration:1000, property:'top', start:80, end: -100 })
+	// .animate('#layer2',{ duration: 1000, property:'top', start:0, end: -100 })
 	// .animate('#layer1',{ duration:1000, property:'top', start:-100, end: 80 })
 	// .animate('#layer2',{ duration: 1000, property:'top', start:-100, end: 0 })
 }
